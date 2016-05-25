5b08a0473cc08932c27f09645b8290ad3f914985
Christina <christina.vandd@gmail.com>
2016-05-25 13:18:18 +0200

    installing some user stuff

--- module/Application/config/autoload/view.php
+++ module/Application/config/autoload/view.php
@@ -25,6 +25,7 @@ return
         'template_path_stack' =>
         [
             __DIR__ . '/../../view',
+            'zfc-user' => __DIR__ . '/../../view'
         ],
     ],
 ];
---
+++ module/Application/src/Application/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chris
+ * Date: 25/05/2016
+ * Time: 12:54
+ */
+
+namespace Application\Entity;
+
+use ZfcUser\Entity\UserInterface;
+
+/**
+ * Class User
+ * @package Application\Entity
+ */
+abstract class User implements UserInterface
+{
+
+}
